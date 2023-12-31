4828d81efe32fa848dffa0763f5fd7508b66a963
lalitsarode2003 <93986123+lalitsarode2003@users.noreply.github.com>
2023-12-31 21:20:40 +0530

    Add files via upload

---
+++ if_else practice/program03.py
@@ -0,0 +1,8 @@
+Write a program to find whether a number is even or odd
+
+
+x = int(input("Enter any no."))
+if x%2 == 0:
+    print(x,"is a even no")
+else:
+    print(x,"is a odd no.")
---
+++ if_else practice/program08.py
@@ -0,0 +1,8 @@
+Write a program to check whether the number is greater than 10 or not
+
+
+x = int(input("Enter any no:"))
+if x>10 :
+    print(x,"is greater than 10")
+else:
+    print(x,"is less than 10")
